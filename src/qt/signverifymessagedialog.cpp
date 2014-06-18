1e0ca694292ef5b16a2ae9cb6e04c789a324a1da
m0gliE <m0gliEz@gmail.com>
2014-06-17 20:29:52 -0700

    Update signverifymessagedialog.cpp

--- src/qt/signverifymessagedialog.cpp
+++ src/qt/signverifymessagedialog.cpp
@@ -28,10 +28,10 @@ SignVerifyMessageDialog::SignVerifyMessageDialog(QWidget *parent) :
 
 #if (QT_VERSION >= 0x040700)
     /* Do not move this to the XML file, Qt before 4.7 will choke on it */
-    ui->addressIn_SM->setPlaceholderText(tr("Enter a Fastcoin address (e.g. Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)"));
+    ui->addressIn_SM->setPlaceholderText(tr("Enter a Fastcoin address (e.g. fpQXGrv3ChSdG9iurnP58wUUmZpqJXRgnN)"));
     ui->signatureOut_SM->setPlaceholderText(tr("Click \"Sign Message\" to generate signature"));
 
-    ui->addressIn_VM->setPlaceholderText(tr("Enter a Fastcoin address (e.g. Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)"));
+    ui->addressIn_VM->setPlaceholderText(tr("Enter a Fastcoin address (e.g. fpQXGrv3ChSdG9iurnP58wUUmZpqJXRgnN)"));
     ui->signatureIn_VM->setPlaceholderText(tr("Enter Fastcoin signature"));
 #endif
 
