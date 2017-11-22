5d085f1693c1c8fc52c301c36da078cf04c32d12
Christopher Tao <CTao89@users.noreply.github.com>
2017-11-22 00:37:53 -0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -2,6 +2,7 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// CORE Update
 
 #include "chainparams.h"
 
