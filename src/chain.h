e7dc231e577d658b443f78c2390a370795a15da9
Chewy83 <33862161+Chewy83@users.noreply.github.com>
2017-11-22 12:55:04 -0500

    Update chain.h

--- src/chain.h
+++ src/chain.h
@@ -2,6 +2,7 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// CORE Update Line 7
 
 #ifndef BITCOIN_CHAIN_H
 #define BITCOIN_CHAIN_H
