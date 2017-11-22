a28a3c4657afbb73f330ea48094cd749bda9f20e
Cheetahx <fastcoin12345@gmail.com>
2017-11-21 23:30:42 -0500

    Update clientmodel.cpp

--- src/qt/clientmodel.cpp
+++ src/qt/clientmodel.cpp
@@ -240,3 +240,4 @@ void ClientModel::unsubscribeFromCoreSignals()
     uiInterface.NotifyNumConnectionsChanged.disconnect(boost::bind(NotifyNumConnectionsChanged, this, _1));
     uiInterface.NotifyAlertChanged.disconnect(boost::bind(NotifyAlertChanged, this, _1, _2));
 }
+
