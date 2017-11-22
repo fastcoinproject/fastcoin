a288f1696b65768fed5d8be9b4ba4d7665d8e58c
Cheetahx <fastcoin12345@gmail.com>
2017-11-21 21:57:28 -0500

    Update base58.h

--- src/base58.h
+++ src/base58.h
@@ -176,3 +176,4 @@ typedef CBitcoinExtKeyBase<CExtKey, 74, CChainParams::EXT_SECRET_KEY> CBitcoinEx
 typedef CBitcoinExtKeyBase<CExtPubKey, 74, CChainParams::EXT_PUBLIC_KEY> CBitcoinExtPubKey;
 
 #endif // BITCOIN_BASE58_H
+
