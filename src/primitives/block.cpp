7101b65c6206495764381869c83726f55480be32
Christopher Tao <CTao89@users.noreply.github.com>
2017-11-22 00:37:28 -0500

    Update block.cpp

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -2,6 +2,7 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// CORE Update
 
 #include "primitives/block.h"
 
